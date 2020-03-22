77e9e4c4fbd58355baeff115c69d636db8bd81d4
mehechi <maxime1jacquet@gmail.com>
2020-03-22 12:10:13 +0100

    feat(favicon)

--- dist/src/scripts/index.d.ts
+++
@@ -1,4 +0,0 @@
-import "../stylesheets/index.scss";
-export interface NewObject {
-    toto: string;
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,69 @@
-const Path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: Path.resolve(__dirname, "./src/scripts/index.ts")
+    app: Path.resolve(__dirname, './src/scripts/index.ts')
   },
 
   output: {
-    path: Path.join(__dirname, "./build"),
-    chunkFilename: "[name].js",
-    filename: "js/[name].js"
+    path: Path.join(__dirname, './build'),
+    chunkFilename: '[name].js',
+    filename: 'js/[name].js'
   },
   optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
       name: false
     }
   },
   plugins: [
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin([
-      { from: Path.resolve(__dirname, "./src/server.js"), to: "server.js" }
+      { from: Path.resolve(__dirname, './src/server.js'), to: 'server.js' }
+    ]),
+    new CopyWebpackPlugin([
+      { from: Path.resolve(__dirname, './src/assets'), to: 'assets' }
     ]),
     new HtmlWebpackPlugin({
-      template: Path.resolve(__dirname, "./src/index.html")
+      template: Path.resolve(__dirname, './src/index.html')
     })
   ],
   resolve: {
     alias: {
-      "~": Path.resolve(__dirname, "./src")
+      '~': Path.resolve(__dirname, './src')
     },
-    extensions: [".ts", ".js"]
+    extensions: ['.ts', '.js']
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: [/node_modules/],
-        loader: "ts-loader"
+        loader: 'ts-loader'
       },
 
       {
         test: /\.mjs$/,
         include: /node_modules/,
-        type: "javascript/auto"
+        type: 'javascript/auto'
       },
       {
         test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
         use: {
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
-            name: "[path][name].[ext]"
+            name: '[path][name].[ext]'
           }
         }
       },
       {
         test: /\.s?css$/i,
-        use: ["style-loader", "css-loader?sourceMap=true", "sass-loader"]
+        use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
       }
     ]
   },
-  devtool: "source-map"
+  devtool: 'source-map'
 };
