55e065d128448d268bce40be59014adec3f077c4
mehechi <maxime1jacquet@gmail.com>
2020-05-24 21:48:07 +0200

    test

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,6 +1,23 @@
 import { App } from './features/app';
 import './sw';
+import { from } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 document.addEventListener('DOMContentLoaded', () => {
   new App();
 });
+
+// Calculate place in localstorage
+// if (navigator.storage && navigator.storage.estimate) {
+//   const quota$: any = from(navigator.storage.estimate());
+//   quota$
+//     .pipe(
+//       map((quota: any) => {
+//         const percentageUsed = (quota.usage / quota.quota) * 100;
+//         console.log(`You've used ${percentageUsed}% of the available storage.`);
+//         const remaining = quota.quota - quota.usage;
+//         console.log(`You can write up to ${remaining} more bytes.`);
+//       })
+//     )
+//     .subscribe();
+// }
